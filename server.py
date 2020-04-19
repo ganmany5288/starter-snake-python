210543257c5631cf2f42a5c21075ac139cc6e44c
James Tang <rhsjamestang2019@gmail.com>
2020-04-19 12:33:09 -0700

    updated snake's look

--- server.py
+++ server.py
@@ -29,7 +29,8 @@ class Battlesnake(object):
         # TODO: Use this function to decide how your snake is going to look on the board.
         data = cherrypy.request.json
         print("START")
-        return {"color": "#888888", "headType": "regular", "tailType": "regular"}
+        print("GET READY TO BE DESTROYED BY THE TIANMING_SNAKE")
+        return {"color": "#888888", "headType": "pixel", "tailType": "bolt"}
 
     @cherrypy.expose
     @cherrypy.tools.json_in()
